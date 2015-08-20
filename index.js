366ec58d34f83e9357f326078d892103ab04d2dc
Paulo Rodrigues Pinto <regularjack@gmail.com>
2015-08-20 20:00:24 +0100

    Change the default environment to development

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function current(env) {
 }
 
 function reset() {
-  current(make(process.env.NODE_ENV || argv.env || "production"));
+  current(make(process.env.NODE_ENV || argv.env || "development"));
 }
 
 function make(name) {
